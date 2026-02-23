e567f57d951ab7ebea94814fa7e6972ad5fa6c6e
chiwireasado <mariajoseramirezperez1@gmail.com>
2026-02-23 11:14:32 +0100

    estan las 4 clases estructuras y he ido jugando con la APIs Open-meteo, el menu esta hecho

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-#menu
-opciones = {
-    '1': ('Modulo global', showTodo),
-    '2': ('Modulo transporte ', showTransporte),
-    '3': ('Modulo geografico', showLugar),
-    '4': ('Modulo temporal', showTiempo),
-}
+from modulo_global import ModuloGlobal
+from modulo_transporte import ModuloTransporte
+from modulo_geo import ModuloGeografico
+from modulo_temporal import ModuloTemporal
+
+
+class RainCheck:
+    def __init__(self):
+        self.m_global = ModuloGlobal()
+        self.m_transporte = ModuloTransporte()
+        self.m_geo = ModuloGeografico()
+        self.m_temporal = ModuloTemporal()
+
+        self.opciones = {
+            '1': ('Modulo global', self.m_global.ejecutar),
+            '2': ('Modulo transporte ', self.m_transporte.ejecutar),
+            '3': ('Modulo geografico', self.m_geo.ejecutar),
+            '4': ('Modulo temporal', self.m_temporal.ejecutar),
+            '5': ('Salir', self.salir)
+        }
+
+def showMenu(self):
+    while True:
+        print("<<<<<<<<< RAIN CHECK MADRID >>>>>>>>>>>>>")
+        for o, (nombre, _) in self.opciones.items():
+            print(f"{o} . {nombre}")
+
+        choice = input("\n Selecciona una opcion: ")
+
+        if choice in self.opciones:
+            self.opciones[choice][1]()
+        else:
+            print("esa opcion no existe, escoge una de verdad")
+
+
+def salir(self):
+    print("Gracias por usar RainCheck. ¡Cuidado con los charcos!")
+    exit()
+
+
+if __name__ == "__main__":
+    app = RainCheck()
+    app.showMenu()
--- modulo_global.py
+++ modulo_global.py
@@ -1,17 +1,22 @@
 import requests
 import pandas as pd
 
-def get_rain_data(lat, lon):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=precipitation&timezone=Europe%2FMadrid"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
 
+class ModuloGlobal:
+    def __init__(self):
+        self.lat_madrid = 40.4168
+        self.lon_madrid = -3.7038
 
-       #lo pone bonito
-        df = pd.DataFrame({
-            'Tiempo': data['hourly']['time'],
-            'Precipitación (mm)': data['hourly']['precipitation']
-        })
-        return df
-    return None
+    def get_rain_data(lat, lon):
+        url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=precipitation&timezone=Europe%2FMadrid"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+
+            # lo nope lindo
+            df = pd.DataFrame({
+                'Tiempo': data['hourly']['time'],
+                'Precipitación (mm)': data['hourly']['precipitation']
+            })
+            return df
+        return None

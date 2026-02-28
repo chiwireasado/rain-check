23a6836c449e59ce63c98b698cb5e22dd594fdc4
chiwireasado <mariajoseramirezperez1@gmail.com>
2026-02-28 17:21:04 +0100

    modulo global sin seguridad pero con bucle

--- modulo_global.py
+++ modulo_global.py
@@ -1,23 +1,79 @@
 import requests
-import pandas as po
-
-
 class ModuloGlobal:
     def __init__(self):
         self.lat_madrid = 40.4168
         self.lon_madrid = -3.7038
 
-    def get_rain_data(lat, lon):
+        self.client_id = "chiwireasado"
+        self.api_key = "Mjjlmp123*"
 
-        url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=precipitation&timezone=Europe%2FMadrid"
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
 
-            # esto es lo que arregla el formato
-            datos = po.DataFrame({
-                'Tiempo': data['hourly']['time'],
-                'Precipitación (mm)': data['hourly']['precipitation']
-            })
-            return datos
-        return None
+    def token(self):
+        url = "https://openapi.emtmadrid.es/v2/mobilitylabs/user/login/"
+        credenciales = {"X-ClientId": self.client_id, "passKey": self.api_key}
+        try:
+            response = requests.get(url, headers=credenciales)
+            return response.json()['data'][0]['accessToken']
+        except:
+            print("no se pudo establecer la conexion")
+            return None
+
+
+    def ejecutar(self):
+        print("\n >>> MÓDULO GLOBAL <<<")
+        hora = input("1. Introduce la hora para el clima (00 a 23): ")
+        parada = input("2. Introduce el número de parada para transporte: ")
+
+        url_clima = f"https://api.open-meteo.com/v1/forecast?latitude={self.lat_madrid}&longitude={self.lon_madrid}&hourly=precipitation&timezone=Europe%2FMadrid&forecast_days=1"
+
+        try:
+            clima = requests.get(url_clima).json()
+            tiempos = clima['hourly']['time']
+            lluvias = clima['hourly']['precipitation']
+
+            look = f"T{hora.zfill(2)}:00"
+            lluvia_contador = 0
+
+            for i in range(len(tiempos)):
+                if look in tiempos[i]:
+                    lluvia_contador = lluvias[i]
+                    break
+
+            tk = self.token()
+            tiempos_bus = []
+
+            if tk:
+                url_emt = f"https://openapi.emtmadrid.es/v2/transport/busemtmad/stops/{parada}/arriving/"
+                credenciales_emt = {"accessToken": tk}
+                payload = {"statistics": "N", "cultureInfo": "ES"}
+
+                res_emt = requests.post(url_emt, headers=credenciales_emt, json=payload).json()
+                llegadas = res_emt['data'][0]['Arrive']
+
+
+                for bus in llegadas:
+                    tiempos_bus.append({
+                        "linea": bus['line'],
+                        "minutos de espera": bus['estimateArrive'] // 60
+                    })
+            else:
+                print("Error de autenticación en EMT")
+
+
+            print(f"LLuvia prevista ({hora}:00): {lluvia_contador}mm")
+            if lluvia_contador > 0:
+                print("Estado: Se recomienda llevar paraguas, hay alerta de lluvia, feliz dia")
+            else:
+                print("Estado: Sin lluvia. guarda el paraguas, feliz día")
+
+            print(f"Autobus en parada: {parada}")
+            if tiempos_bus:
+                for bus in tiempos_bus:
+                    print(f" > Linea {bus['linea']}: llegada en {bus['minutos']} min")
+            else:
+                print("No hay informacion de bus")
+
+        except Exception as a:
+            print(f"error en el modulo global. Error:{a}")
+
+

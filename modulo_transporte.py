1cd7d914f12077b8e64ce78269216a20efd8c575
chiwireasado <mariajoseramirezperez1@gmail.com>
2026-03-01 22:45:08 +0100

    modulo transporte

--- modulo_transporte.py
+++ modulo_transporte.py
@@ -4,29 +4,31 @@ class ModuloTransporte:
         def __init__(self):
             self.client_id = "chiwireasado"
             self.api_key = "Mjjlmp123*"
-            self.base_url = "https://openapi.emtmadrid.es/v2/transport/busemtmad/stops"
+
 
         def login(self):
             url = "https://openapi.emtmadrid.es/v2/mobilitylabs/user/login/"
-            headers = {"X-ClientId": self.client_id, "passKey": self.api_key}
+            credenciales = {"X-ClientId": self.client_id, "passKey": self.api_key}
             try:
-                response = requests.get(url, headers=headers)
+                response = requests.get(url, headers=credenciales)
                 return response.json()['data'][0]['accessToken']
             except:
+                print("No se pudo conectar con la EMT")
                 return None
 
 
         def ejecutar(self):
-            print("\n--- [MÓDULO TRANSPORTE EN TIEMPO REAL] ---")
-            stop_id = input("Introduce el número de la parada (ej: 7265): ")
+            print("\n>>>> [MÓDULO TRANSPORTE EN TIEMPO REAL <<<<")
+            stop_id = input("Introduce el número de parada exacto")
 
-            token = self.login()
-            if not token:
+            tk = self.token()
+
+            if not tk:
                 print("Error: No se pudo autenticar con la EMT.")
                 return
 
             url = f"{self.base_url}/{stop_id}/arriving/"
-            headers = {"accessToken": token}
+            headers = {"accessToken": tk}
 
 
             payload = {"statistics": "N", "cultureInfo": "ES"}
